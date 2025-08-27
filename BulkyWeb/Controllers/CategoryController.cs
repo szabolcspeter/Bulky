c8aa86b2073f81c55bfc57bf2bf9d9f232ba8fdb
Szabolcs Péter <szabi.peter@gmail.com>
2025-08-27 14:09:01 +0200

    Display category data

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categoryList = _context.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
